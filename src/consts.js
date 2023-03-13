1aade0b7b776c425720c8f809bc763ec50440973
duanzhicheng200506 <duanzhicheng200506@credithc.com>
2023-03-13 18:52:46 +0800

    update

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,6 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+
+export const SITE_TITLE = `UncleLLD'Blog`;
+export const SITE_DESCRIPTION = 'UncleLLD Site Description';
+export const SITE_EMAIL = 'dacheng2699@gmail.com'
+export const SITE_NAME = 'UncleLLD';
+export const SITE_URL = "https://blog-one-blue.vercel.app/";
